0b99a83743dc156b83ceb8738b26140026b20806
Vincent Incarvite <vinniemac189@gmail.com>
2017-09-22 23:48:52 -0400

    Added AbilityScoreModel

---
+++ public/js/model/abilityScoreModel.js
@@ -0,0 +1,27 @@
+var _ = require('underscore');
+var BaseModel = require('./baseModel');
+
+var AbilityScoreModel = BaseModel.extend({
+    defaults: _.extend({}, BaseModel.defaults, {
+        value: null
+    }),
+
+    initialize: function(attrs, options) {},
+
+    getModifier: function() {
+        var value = _.isNumber(this.getValue()) ? this.getValue() : AbilityScoreModel.defaultValue;
+        return Math.floor((value - 10) / 2);
+    },
+
+    getValue: function() {
+        return this.getValue(AbilityScoreModel.fields.VALUE);
+    }
+},{
+    defaultValue: 8,
+
+    fields: _.extend({}, BaseModel.fields, {
+        VALUE: 'value'
+    })
+});
+
+module.exports = AbilityScoreModel;
