3b1da9f84a3109f9719648fd018839a0a63362ea
Vincent Incarvite <vinniemac189@gmail.com>
2017-10-05 23:58:30 -0400

    Modified the ModelParserController to be more generic for other classes to use

--- public/js/controller/modelParserController.js
+++ public/js/controller/modelParserController.js
@@ -3,24 +3,37 @@ var ClassModel = require('../model/classModel');
 var RaceModel = require('../model/raceModel');
 
 var ModelParserController = {
+
+    parseBackground: function(json) {
+        return new BackgroundModel(json);
+    },
+
     parseBackgrounds: function(json) {
-        var models = _.map(json.backgrounds, function(modelJson) {
-            return new BackgroundModel(modelJson);
-        });
+        var arr = _.isObject(json) ? json.backgrounds : json;
+        arr = _.isArray(arr) ? arr : [];
+        var models = _.map(arr, ModelParserController.parseBackground);
         return { backgrounds: models };
     },
 
+    parseClass: function(json) {
+        return new ClassModel(json);
+    },
+
     parseClasses: function(json) {
-        var models = _.map(json.classes, function(modelJson) {
-            return new ClassModel(modelJson);
-        });
+        var arr = _.isObject(json) ? json.classes : json;
+        arr = _.isArray(arr) ? arr : [];
+        var models = _.map(arr, ModelParserController.parseClass);
         return { classes: models };
     },
 
+    parseRace: function(json) {
+        return new RaceModel(json);
+    },
+
     parseRaces: function(json) {
-        var models = _.map(json.races, function(modelJson) {
-            return new RaceModel(modelJson);
-        });
+        var arr = _.isObject(json) ? json.races : json;
+        arr = _.isArray(arr) ? arr : [];
+        var models = _.map(arr, ModelParserController.parseRace);
         return { races: models };
     }
 };
