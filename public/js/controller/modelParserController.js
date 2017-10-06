dc927e2b9b61151e530e91a4b4facf10aab949f9
Vincent Incarvite <vinniemac189@gmail.com>
2017-10-06 00:05:10 -0400

    Added parser functions for each model

--- public/js/controller/modelParserController.js
+++ public/js/controller/modelParserController.js
@@ -1,6 +1,14 @@
 var BackgroundModel = require('../model/backgroundModel');
+var ChoiceModel = require('../model/choiceModel');
 var ClassModel = require('../model/classModel');
+var ConditionModel = require('../model/conditionModel');
+var FeatureModel = require('../model/featureModel');
+var ProficiencyModel = require('../model/proficiencyModel');
 var RaceModel = require('../model/raceModel');
+var SpellCastingModel = require('../model/spellCastingModel');
+var SpellModel = require('../model/spellModel');
+var SpellSlotsModel = require('../model/spellSlotsModel');
+var UnlockableModel = require('../model/unlockableModel');
 
 var ModelParserController = {
 
@@ -15,6 +23,10 @@ var ModelParserController = {
         return { backgrounds: models };
     },
 
+    parseChoice: function(json) {
+        return new ChoiceModel(json);
+    },
+
     parseClass: function(json) {
         return new ClassModel(json);
     },
@@ -26,6 +38,18 @@ var ModelParserController = {
         return { classes: models };
     },
 
+    parseCondition: function(json) {
+        return new ConditionModel(json);
+    },
+
+    parseFeature: function(json) {
+        return new FeatureModel(json);
+    },
+
+    parseProficiency: function(json) {
+        return new ProficiencyModel(json);
+    },
+
     parseRace: function(json) {
         return new RaceModel(json);
     },
@@ -35,6 +59,22 @@ var ModelParserController = {
         arr = _.isArray(arr) ? arr : [];
         var models = _.map(arr, ModelParserController.parseRace);
         return { races: models };
+    },
+
+    parseSpellCasting: function(json) {
+        return new SpellCastingModel(json);
+    },
+
+    parseSpell: function(json) {
+        return new SpellModel(json);
+    },
+
+    parseSpellSlots: function(json) {
+        return new SpellSlotsModel(json);
+    },
+
+    parseUnlockable: function(json) {
+        return new UnlockableModel(json);
     }
 };
 
