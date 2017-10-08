e104ae6216a39a1a83d13c7f6c967203dc1fdbb7
Vincent Incarvite <vinniemac189@gmail.com>
2017-10-07 21:26:13 -0400

    Parse UnlockableModel attributes and remove choices & unlockables from FeatureModel (prevents circular dependencies)

--- public/js/model/featureModel.js
+++ public/js/model/featureModel.js
@@ -1,32 +1,8 @@
-var ChoiceCollection = require('../collection/choiceCollection');
-var UnlockableCollection = require('../collection/unlockableCollection');
-
 var FeatureModel = Backbone.Model.extend({
     defaults: {
-        choices: null,          //ChoiceCollection
         description: [],        //List of Strings
         name: null,             //String
-        shortDescription: null, //String
-        unlockables: null       //UnlockableCollection
-    },
-
-    initialize: function(attrs, options) {
-        attrs = attrs || {};
-
-        var choiceModels = _.map(attrs.choices, ChoiceCollection.parseModel) || [];
-        this.set(FeatureModel.fields.CHOICES, new ChoiceCollection(choiceModels));
-
-        var unlockableModels = _.map(attrs.unlockables, UnlockableCollection.parseModel) || [];
-        this.set(FeatureModel.fields.UNLOCKABLES, new UnlockableCollection(unlockableModels));
-    },
-
-    getChoices: function() {
-        return this.get(FeatureModel.fields.CHOICES);
-    },
-
-    setChoices: function(choiceModels) {
-        this.getChoices().reset(choiceModels || []);
-        return this;
+        shortDescription: null  //String
     },
 
     getDescription: function() {
@@ -39,23 +15,12 @@ var FeatureModel = Backbone.Model.extend({
 
     getShortDescription: function() {
         return this.get(FeatureModel.fields.SHORT_DESCRIPTION);
-    },
-
-    getUnlockables: function() {
-        return this.get(FeatureModel.fields.UNLOCKABLES);
-    },
-
-    setUnlockables: function(unlockableModels) {
-        this.getUnlockables().reset(unlockableModels || []);
-        return this;
     }
 },{
     fields: {
-        CHOICES: 'choices',
         DESCRIPTION: 'description',
         NAME: 'name',
-        SHORT_DESCRIPTION: 'shortDescription',
-        UNLOCKABLES: 'unlockables'
+        SHORT_DESCRIPTION: 'shortDescription'
     }
 });
 
--- public/js/model/unlockableModel.js
+++ public/js/model/unlockableModel.js
@@ -1,23 +1,58 @@
-var UnlockableModel = Backbone.Model.extend({
+var AbilityScoreAdjustmentCollection = require('../collection/abilityScoreAdjustmentCollection');
+var ChoiceCollection = require('../collection/choiceCollection');
+var ConditionModel = require('../model/conditionModel');
+var FeatureCollection = require('../collection/featureCollection');
+var ProficiencyCollection = require('../collection/proficiencyCollection');
+
+var UnlockableModel = UnlockableModel = Backbone.Model.extend({
     defaults: {
-        abilityScoreAdjustments: [],//List of AbilityScoreAdjustmentModels
-        choices: [],                //List of ChoiceModels
-        condition: null,            //ConditionModel
-        features: [],               //List of FeatureModels
-        proficiencies: [],          //List of ProficiencyModels
-        speed: null                 //Number
+        abilityScoreAdjustments: null,  //AbilityScoreAdjustmentCollection
+        choices: null,                  //ChoiceCollection
+        condition: null,                //ConditionModel
+        features: null,                 //FeatureCollection
+        proficiencies: null,            //ProficiencyCollection
+        speed: null                     //Number
     },
 
     initialize: function(attrs, options) {
-        //TODO: parse some parts into models
+        attrs = attrs || {};
+
+        var abilityScoreAdjustmentModels = _.map(attrs.abilityScoreAdjustments,
+            AbilityScoreAdjustmentCollection.parseModel) || [];
+        this.set(UnlockableModel.fields.ABILITY_SCORE_ADJUSTMENTS,
+            new AbilityScoreAdjustmentCollection(abilityScoreAdjustmentModels));
+
+        var choiceModels = _.map(attrs.choices, ChoiceCollection.parseModel) || [];
+        this.set(UnlockableModel.fields.CHOICES, new ChoiceCollection(choiceModels));
+
+        if (!_.isEmpty(attrs.condition)) {
+            var conditionModel = new ConditionModel(attrs.condition);
+            this.set(UnlockableModel.fields.CONDITION, conditionModel);
+        }
+
+        var featureModels = _.map(attrs.features, FeatureCollection.parseModel) || [];
+        this.set(UnlockableModel.fields.FEATURES, new FeatureCollection(featureModels));
+
+        var proficiencyModels = _.map(attrs.proficiencies, ProficiencyCollection.parseModel) || [];
+        this.set(UnlockableModel.fields.PROFICIENCIES, new ProficiencyCollection(proficiencyModels));
     },
 
     getAbilityScoreAdjustments: function() {
         return this.get(UnlockableModel.fields.ABILITY_SCORE_ADJUSTMENTS);
     },
 
+    setAbilityScoreAdjustments: function(abilityScoreAdjustmentModels) {
+        this.getAbilityScoreAdjustments().reset(abilityScoreAdjustmentModels || []);
+        return this;
+    },
+
     getChoices: function() {
         return this.get(UnlockableModel.fields.CHOICES);
+    },
+
+    setChoices: function(choiceModels) {
+        this.getChoices().reset(choiceModels || []);
+        return this;
     },
 
     getCondition: function() {
@@ -28,8 +63,18 @@ var UnlockableModel = Backbone.Model.extend({
         return this.get(UnlockableModel.fields.FEATURES);
     },
 
+    setFeatures: function(featureModels) {
+        this.getFeatures().reset(featureModels || []);
+        return this;
+    },
+
     getProficiencies: function() {
         return this.get(UnlockableModel.fields.PROFICIENCIES);
+    },
+
+    setProficiencies: function(proficiencyModels) {
+        this.getProficiencies().reset(proficiencyModels || []);
+        return this;
     },
 
     getSpeed: function() {
