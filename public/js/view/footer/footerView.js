a75224c40ef3fab891f5afc0a20d91d8a5d7f64b
Vincent Incarvite <vinniemac189@gmail.com>
2017-11-03 00:08:16 -0400

    Fixed some bugs; Updated the d20 image; Styled the error page

--- public/js/view/footer/footerView.js
+++ public/js/view/footer/footerView.js
@@ -1,7 +1,16 @@
+var LoadingView = require('../loading/loadingView');
 var templates = require('./footerView.html');
 
 var FooterView = Backbone.View.extend({
     className: 'page-container footer-view',
+
+    events: {
+        'click .d20-span': 'onClickD20'
+    },
+
+    onClickD20: function(e) {
+        LoadingView.show('How did you know to click on me!?');
+    },
 
     render: function() {
         var html = templates.scaffold();
